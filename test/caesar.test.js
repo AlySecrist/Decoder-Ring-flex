521f8154a9b9fb1d349f4233c918c65e901afcd4
Alyssa Secrist <alyssa.secrist@gmail.com>
2021-03-25 11:26:57 -0500

    polybius test stubs written

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -2,14 +2,6 @@
 
 // Write your tests here!
 
-/*
-Caesar shift:
-[x] It returns false if the shift value is equal to 0, less than -25, greater than 25, or not present.
-[x] It ignores capital letters. (For example, the results of A Message and a message should be the same.)
-[x] When encoding, it handles shifts that go past the end of the alphabet. (For example, shifting z to the right by 3 should cause the z to wrap around to the front of the alphabet, so that z becomes c.)
-[x] It maintains spaces and other nonalphabetic symbols in the message, before and after encoding or decoding.
-*/
-
 const expect = require('chai').expect;
 const caesarModule = require('../src/caesar');
 
@@ -17,9 +9,13 @@ const caesarModule = require('../src/caesar');
 
 describe('caesarModule.caesar ENCODING', () => {
   it('should return false if shift value is 0, < -25, > 25, or not provided', () => {
-    const actual = caesarModule.caesar();
+    const test0 = caesarModule.caesar();
+    const testN25 = caesarModule.caesar();
+    const test25 = caesarModule.caesar();
 
-    expect(actual).to.be.false;
+    expect(test0).to.be.false;
+    expect(testN25).to.be.false;
+    expect(test25).to.be.false;
   });
 
   it('should treat capital letters as lower case', () => {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,3 +1,80 @@
 'use strict';
 
 // Write your tests here!
+
+/*
+Polybius square:
+[x] When encoding, it translates the letters i and j to 42. -->
+[x] When decoding, it translates 42 to (i/j). -->
+[x] It ignores capital letters. (For example, the results of A Message and a message should be the same.) -->
+[x] It maintains spaces in the message, before and after encoding or decoding. -->
+*/
+
+const expect = require('chai').expect;
+const polybiusModule = require('../src/polybius');
+
+//*********************************** Encoding plain messages ***********************************
+
+describe('polybiusModule.polybius ENCODING', () => {
+  it('should translate i and j to 42', () => {
+    const actual = polybiusModule.polybius();
+    const expected = '';
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('should treat capital letters as lower case.', () => {
+    const actual = polybiusModule.polybius();
+    const expected = '';
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('should not alter spaces', () => {
+    const actual = polybiusModule.polybius();
+    const expected = '';
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('should return an even number of number characters for each encoded word', () => {
+    const actual = polybiusModule.polybius().length;
+    const expected = 2;
+
+    expect(actual).to.equal(expected);
+  });
+});
+
+
+// =============================================================================================
+// ***************************** Decoding already encoded messages *****************************
+
+describe('polybiusModule.polybius DECODING', () => {
+  it('should translate 42 to both i and j (i/j)', () => {
+    const actual = polybiusModule.polybius();
+    const expected = '';
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('should return all lower case.', () => {
+    const actual = polybiusModule.polybius();
+    const expected = '';
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('should not alter spaces', () => {
+    const actual = polybiusModule.polybius();
+    const expected = '';
+
+    expect(actual).to.equal(expected);
+  });
+
+  it('should return flase if the number of number characters for each encoded word is not even', () => {
+    const actual = polybiusModule.polybius().length;
+    const expected = 2;
+
+    expect(actual).to.equal(expected);
+  });
+});
