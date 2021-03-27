104c404073dd585e41eccdf2ae6d60f9b56de465
Alyssa Secrist <alyssa.secrist@gmail.com>
2021-03-27 00:01:02 -0500

    caesar encoding passes tests

--- src/caesar.js
+++ src/caesar.js
@@ -5,11 +5,45 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 8
 
+function encodeCharacter(letter, alphabet, shift) {
+  for (let i = 0; i < alphabet.length; i ++) {
+    if (letter === alphabet[i]) {
+      let encoded = i + shift;
+
+      if (encoded > 25) {
+        encoded = -1 + (encoded - 25);
+      } else if (encoded < 0) {
+
+        encoded = 26 + encoded;
+      }
+      
+      return alphabet[encoded];
+    }
+  }
+}
+
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
   function caesar(input, shift, encode = true) {
     // your solution code here
+    
+    if (!shift || shift === 0 || shift <= -25 || shift >= 25) return false;
+
+    let result = '';
+
+    if (encode === true) {
+      for (let i = 0; i < input.length; i++) {
+        const letter = input[i].toLowerCase();
+
+        if (!letter.match(/[a-z]/i)) {
+          result += letter;
+        } else result += encodeCharacter(letter, alphabet, shift);
+      }
+    }
+    return result;
   }
 
   return {
